9c5ac50fb78dd13d71cf3caca88b6a6281863c09
phi <phi1618jp@gmail.com>
2012-04-21 20:47:41 +0900

    google code から vector3.js を移植

--- src/geom/vector3.js
+++ src/geom/vector3.js
@@ -1,5 +1,5 @@
 /*
- * vector2.js
+ * vector3.js
  */
 
 /*
@@ -11,9 +11,9 @@ tm.geom = tm.geom || {};
 (function() {
     
     /**
-     * 2次元ベクトル
+     * 3次元ベクトル
      */
-    tm.geom.Vector2 = tm.createClass({
+    tm.geom.Vector3 = tm.createClass({
         /**
          * @cfg {Number}    X 座標
          */
@@ -22,30 +22,40 @@ tm.geom = tm.geom || {};
          * @cfg {Number}    Y 座標
          */
         y: 0,
+        /**
+         * @cfg {Number}    Z 座標
+         */
+        z: 0,
         
+
         /**
          * @constructor
-         * 初期化
+         * ３次元ベクトル
+         * 
+         * - [Test Program](http://tmlib-js.googlecode.com/svn/trunk/test/geom/vector-test.html)
          */
-        init: function(x, y) {
-            this.set(x, y);
+        init: function(x, y, z) {
+            this.set(x, y, z);
         },
         
         /**
-         * セッター
+         * セット
          */
-        set: function(x, y) {
+        set: function(x, y, z) {
             this.x = x;
             this.y = y;
+            this.z = z;
+            
+            return this;
         },
-        
         
         /**
          * 数値からセット
          */
-        setFromNumber: function(x, y) {
+        setFromNumber: function(x, y, z) {
             this.x = x;
             this.y = y;
+            this.z = z;
             
             return this;
         },
@@ -56,6 +66,7 @@ tm.geom = tm.geom || {};
         setFromArray: function(arr) {
             this.x = arr[0];
             this.y = arr[1];
+            this.z = arr[2];
             
             return this;
         },
@@ -66,6 +77,7 @@ tm.geom = tm.geom || {};
         setFromObject: function(obj) {
             this.x = obj.x;
             this.y = obj.y;
+            this.z = obj.z;
             
             return this;
         },
@@ -74,42 +86,72 @@ tm.geom = tm.geom || {};
          * 文字列からセット
          */
         setFromString: function(str) {
-            var m = str.match(/(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?)/);
+            var m = str.match(/(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?)/);
             this.x = parseFloat(m[1]);
             this.y = parseFloat(m[3]);
+            this.z = parseFloat(m[5]);
             
             return this;
         },
         
-
         /**
          * 角度(radian)と長さでベクトルをセット
          */
-        setFromAngle: function(radian, len) {
+        setFromAngle: function(thetaRad, phiRad, len) {
             len = len || 1;
-            this.x = Math.cos(radian)*len;
-            this.y = Math.sin(radian)*len;
+            
+            this.x = len * Math.cos(thetaRad) * Math.sin(phiRad);
+            this.y = len * Math.sin(thetaRad);
+            this.z = len * Math.cos(thetaRad) * Math.cos(phiRad);
+            
             return this;
         },
         
         /**
          * 角度(radian)と長さでベクトルをセット
          */
-        setFromRadian: function(radian, len) {
+        setFromRadian: function(thetaRad, phiRad, len) {
+            return this.setFromAngle(thetaRad, phiRad, len);
+        },
+        
+        /**
+         * 角度(degree)と長さでベクトルをセット
+         */
+        setFromDegree: function(thetaDegree, phiDegree, len) {
+            return this.setFromAngle(thetaDegree*Math.PI/180, phiDegree*Math.PI/180, len);
+        },
+
+        /**
+         * 角度(radian)と長さでベクトルをセット
+         */
+        setFromAngle2D: function(radian, len) {
             len = len || 1;
             this.x = Math.cos(radian)*len;
             this.y = Math.sin(radian)*len;
+            this.z = 0;
+            return this;
+        },
+        
+        /**
+         * 角度(radian)と長さでベクトルをセット
+         */
+        setFromRadian2D: function(radian, len) {
+            len = len || 1;
+            this.x = Math.cos(radian)*len;
+            this.y = Math.sin(radian)*len;
+            this.z = 0;
             return this;
         },
         
         /**
          * 角度(degree)と長さでベクトルをセット
          */
-        setFromDegree: function(degree, len) {
+        setFromDegree2D: function(degree, len) {
             len = len || 1;
             radian = degree * Math.PI/180;
             this.x = Math.cos(radian)*len;
             this.y = Math.sin(radian)*len;
+            this.z = 0;
             return this;
         },
                 
@@ -119,25 +161,29 @@ tm.geom = tm.geom || {};
         setSmart: function(x, y, z) {
             var v = arguments[0];
             // xyz
-            if (arguments.length === 2) {
+            if (arguments.length === 3) {
                 this.x = x;
                 this.y = y;
+                this.z = z;
             }
             // Array
             else if (v instanceof Array) {
                 this.x = v[0];
                 this.y = v[1];
+                this.z = v[2];
             }
             // Object
             else if (v instanceof Object) {
                 this.x = v.x;
                 this.y = v.y;
+                this.z = v.z;
             }
             // String
             else if (typeof(v) == "string") {
-                var m = str.match(/(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?)/);
+                var m = v.match(/(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?),\s*(-?\d+(\.{1}\d+)?)/);
                 this.x = parseFloat(m[1]);
                 this.y = parseFloat(m[3]);
+                this.z = parseFloat(m[5]);
             }
             
             return this;
@@ -149,6 +195,7 @@ tm.geom = tm.geom || {};
         add: function(v) {
             this.x += v.x;
             this.y += v.y;
+            this.z += v.z;
             
             return this;
         },
@@ -159,6 +206,7 @@ tm.geom = tm.geom || {};
         sub: function(v) {
             this.x -= v.x;
             this.y -= v.y;
+            this.z -= v.z;
             
             return this;
         },
@@ -169,6 +217,7 @@ tm.geom = tm.geom || {};
         mul: function(n) {
             this.x *= n;
             this.y *= n;
+            this.z *= n;
             
             return this;
         },
@@ -180,6 +229,7 @@ tm.geom = tm.geom || {};
             console.assert(n != 0, "0 division!!");
             this.x /= n;
             this.y /= n;
+            this.z /= n;
             
             return this;
         },
@@ -190,6 +240,7 @@ tm.geom = tm.geom || {};
         negate: function() {
             this.x = -this.x;
             this.y = -this.y;
+            this.z = -this.z;
             
             return this;
         },
@@ -199,7 +250,7 @@ tm.geom = tm.geom || {};
          * or magnitude
          */
         length: function() {
-            return Math.sqrt(this.x*this.x + this.y*this.y);
+            return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
         },
         
         /**
@@ -208,7 +259,7 @@ tm.geom = tm.geom || {};
          * or lengthSquare or lengthSqrt
          */
         lengthSquared: function() {
-            return this.x*this.x + this.y*this.y;
+            return this.x*this.x + this.y*this.y + this.z*this.z;
         },
         
         /**
@@ -221,20 +272,35 @@ tm.geom = tm.geom || {};
             return this;
         },
         
+
+        toVector2: function() {
+            // TODO:
+        },
+        
+
         /**
          * 角度(radian)に変換
          */
-        toAngle: function() {
+        toAngleXY: function() {
             return Math.atan2(this.y, this.x);
         },
         
+        
+        /**
+         * 3D化する
+         */
+        to3D: function() {
+            // TODO: 3d化する
+        },
+        
         toStyleString: function() {
-            return "{x:{x}, y:{y}}".format(this);
+            return "{x:{x}, y:{y}, z:{z}}".format(this);
         },
         
         toString: function() {
-            return "{x:{x}, y:{y}}".format(this);
+            return "{x:{x}, y:{y}, z:{z}}".format(this);
         },
+        
         
         /**
          * X値をセット
@@ -251,6 +317,15 @@ tm.geom = tm.geom || {};
          */
         setY: function(y) {
             this.y = y;
+            return this;
+        },
+        
+        /**
+         * Z値をセット
+         * チェーンメソッド用セッター
+         */
+        setZ: function(z) {
+            this.z = z;
             return this;
         }
     });
