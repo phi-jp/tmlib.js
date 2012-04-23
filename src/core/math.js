4d122587204ec2b8142ea48b761bd052177627f9
phi <phi1618jp@gmail.com>
2012-04-24 00:14:48 +0900

    tm.dom.Event クラスを移植. Math に rand, randf 関数を追加

--- src/core/math.js
+++ src/core/math.js
@@ -1,3 +1,30 @@
 /*
  * math.js
  */
+
+
+(function() {
+    
+    /**
+     * @class Math
+     * 数学
+     */
+    
+    /**
+     * @method
+     * ランダムな値を指定された範囲内で生成
+     */
+    Math.rand = function(min, max) {
+        return window.Math.floor( Math.randf(min, max) );
+    };
+    
+    /**
+     * @method
+     * ランダムな値を指定された範囲内で生成
+     */
+    Math.randf= function(min, max) {
+        return window.Math.random()*(max-min)+min;
+    };
+    
+})();
+
--- src/dom/event.js
+++ src/dom/event.js
@@ -2,6 +2,8 @@
  * evnet.js
  */
 
+tm.dom = tm.dom || {};
+
 (function() {
     
     /**
@@ -116,7 +118,171 @@
 
 
 
-
+(function() {
+    
+    /**
+     * @class
+     * Event クラス
+     */
+    tm.dom.Event = tm.createClass({
+        element     : null,
+        funcList    : null,
+        funcIndex   : 0,
+        
+        
+        /**
+         * 初期化
+         */
+        init: function(element) {
+            this.element = element;
+            this.funcList = {};
+        },
+        
+        /**
+         * イベントを追加
+         */
+        add: function(type, fn, id) {
+            var self = this;
+            var elm  = tm.dom.Element(this.element);
+            
+            var temp_fn = function(e) {
+                // return fn.apply(self, arguments);
+                var result = fn.apply(elm, arguments);
+                
+                if (result === false) {
+                    // デフォルトイベントをキャンセル
+                    e.preventDefault();
+                    e.returnValue = false;  // IE
+                    // イベント伝達をキャンセル
+                    e.stopPropagation();
+                }
+                
+                return result;
+            }
+            
+            this._funcIndex = this._funcIndex || 0;
+            id = id || this._funcIndex++;
+            this.funcList[type] = this.funcList[type] || {};
+            this.funcList[type][id] = temp_fn;
+            fn._id = id;    // しれっと記録
+            
+            this.element.addEventListener(type, temp_fn, false);
+            return this;
+        },
+        
+        /**
+         * イベントを解除
+         */
+        remove: function(type, fn_or_id) {
+            var id = (typeof(fn_or_id) === "function") ? fn_or_id._id : fn_or_id;
+            var fn = this.getFunc(type, id);
+            
+            this.element.removeEventListener(type, fn, false);
+            delete this.funcList[type][id];
+        },
+        
+        /**
+         * クリックイベント
+         */
+        click: function(fn, id) {
+            this.add("click", fn, id);
+            return this;
+        },
+        
+        mdlclick: function(fn, id) {
+            var temp_fn = function(e) {
+                if (e.button == 1) {
+                    fn(e);
+                }
+            }
+            this.add("click", temp_fn, id);
+        },
+        
+        /**
+         * ポインティング
+         */
+        pointstart: function(fn, id) {
+            this.add(TM.DOM.Event.POINT_START, fn, id);
+        },
+        pointmove: function(fn, id) {
+            this.add(TM.DOM.Event.POINT_MOVE, fn, id);
+        },
+        pointend: function(fn, id) {
+            this.add(TM.DOM.Event.POINT_END, fn, id);
+        },
+        
+        /**
+         * ホバーイベント
+         */
+        hover: function(fn, id) {
+            this.add("mouseover", fn, id);
+            return this;
+        },
+        
+        /**
+         * 一度だけ呼ばれるイベントを登録
+         */
+        one: function(type, fn, id) {
+            var self = this;
+            var elm  = TM.$DOMElement(this.element);
+            
+            var temp_fn = function() {
+                var result = fn.apply(elm, arguments);
+                self.remove(type, temp_fn);
+                return result;
+            };
+            
+            this.add(type, temp_fn, id);
+            
+            return this;
+        },
+        
+        /**
+         * トグルイベント登録
+         */
+        toggle: function(type, fn_list) {
+            var self = this;
+            var elm  = TM.$DOMElement(this.element);
+            var temp_list = [];
+            
+            for (var i=0; i<fn_list.length; ++i) {
+                var temp_fn = (function(i){
+                    return function(){
+                        var result = fn_list[i].apply(elm, arguments);
+                        
+                        if (result !== false) {
+                            var index = (i+1)%fn_list.length;
+                            self.one(type, temp_list[index]);
+                        }
+                    }
+                })(i);
+                temp_list.push(temp_fn);
+            }
+            
+            this.one(type, temp_list[0]);
+            
+            return this;
+        },
+        
+        /**
+         * 指定したイベントタイプ & id の関数を取得
+         */
+        getFunc: function(type, id) {
+            return this.funcList[type][id];
+        },
+        
+    });
+    
+    
+    /**
+     * @property    event
+     * スタイルクラス
+     */
+    tm.dom.Element.prototype.getter("event", function(){
+        return this._event || ( this._event = Event(this.element) );
+    });
+    
+})();
 
 
 
