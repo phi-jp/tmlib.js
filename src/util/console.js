9de3db00c30212f6765c64d579a59066e6e70d14
phi <phi1618jp@gmail.com>
2012-04-24 02:41:05 +0900

    tm.DebugConsole を tm.util.Console として移植

---
+++ src/util/console.js
@@ -0,0 +1,54 @@
+/*
+ * debug
+ */
+
+tm.util = tm.util || {};
+
+
+(function() {
+    
+    /**
+     * @class
+     * デバッグ用コンソール. 標準である console の dom 版.
+     */
+    tm.util.Console = tm.createClass({
+        
+        element: null,
+        indent: "",
+        
+        init: function(arg) {
+            if (typeof arg == "string") {
+                this.element = document.querySelector(arg);
+            }
+            else {
+                this.element = arg;
+            }
+            this.element.style.whiteSpace = "pre";
+        },
+        
+        clear: function() {
+            this.element.innerHTML = "";
+            return this;
+        },
+        
+        log: function() {
+            var str = Array.prototype.join.call(arguments, ' ');
+            str = str.split('\n').join('\n    ');
+            this.element.innerHTML += this.indent + str + '\n';
+            return this;
+        },
+        
+        group: function(name) {
+            this.element.innerHTML += "<p>{0}</p>".format(name);
+            this.indent += "    ";
+            return this;
+        },
+        
+        groupEnd: function() {
+            this.indent = "";
+        }
+        
+    });
+    
+})();
+
