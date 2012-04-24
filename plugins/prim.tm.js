4f8d582190eabec33a4a5c15acdc1f7a156bba47
phi <phi1618jp@gmail.com>
2012-04-24 23:29:56 +0900

    円や矩形, スターを表示するキャンバスエレメントクラスを定義

---
+++ plugins/prim.tm.js
@@ -0,0 +1,80 @@
+/*
+ * prim.tm.js
+ */
+
+tm.prim = tm.prim || {};
+
+
+(function() {
+    
+    /**
+     *  @class
+     * サークルスプライト 
+     */
+    tm.prim.CircleSprite = tm.createClass({
+        superClass: tm.app.CanvasElement,
+        
+        init: function(radius, color) {
+            this.superInit();
+            
+            this.radius = radius;
+            this.color = color;
+        },
+        
+        draw: function(c) {
+            c.globalCompositeOperation = "lighter";
+            c.fillStyle = this.color;
+            c.fillCircle(0, 0, this.radius);
+        }
+    });
+    
+    
+    /**
+     *  @class
+     * レクトスプライト 
+     */
+    tm.prim.RectSprite = tm.createClass({
+        superClass: tm.app.CanvasElement,
+        
+        init: function(width, height, color) {
+            this.superInit();
+            
+            this.width = width;
+            this.height = height;
+            this.color = color;
+        },
+        
+        draw: function(c) {
+            c.globalCompositeOperation = "lighter";
+            c.fillStyle = this.color;
+            c.fillRect(0, 0, this.width, this.height);
+        }
+    });
+    
+    
+    /**
+     *  @class
+     * スタースプライト 
+     */
+    tm.prim.StarSprite = tm.createClass({
+        superClass: tm.app.CanvasElement,
+        
+        init: function(radius, sides, sideIndent, offsetAngle, color) {
+            this.superInit();
+            
+            this.radius = radius;
+            this.sides = sides;
+            this.sideIndent = sideIndent;
+            this.offsetAngle = offsetAngle;
+            this.color = color;
+        },
+        
+        draw: function(c) {
+            c.globalCompositeOperation = "lighter";
+            c.fillStyle = this.color;
+            c.fillStar(0, 0, this.radius, this.sides, this.sideIndent, this.offsetAngle);
+        }
+    });
+    
+})();
+
