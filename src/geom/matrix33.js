2faf3b53643afee001f17df3a7368e561336e178
phi <phi1618jp@gmail.com>
2012-04-25 01:00:30 +0900

    行列クラス追加

---
+++ src/geom/matrix33.js
@@ -0,0 +1,155 @@
+/*
+ * matrix33.js
+ */
+
+tm.geom = tm.geom || {};
+
+(function() {
+    
+    /**
+     * @class
+     * 3*3 マトリックスクラス
+     */
+    tm.geom.Matrix33 = tm.createClass({
+        /**
+         * 要素
+         */
+        m: null,
+        
+        /**
+         * 初期化
+         */
+        init: function() {
+            this.m = [];
+            if (arguments.length >= 16) {
+                this.set.apply(this, arguments);
+            }
+            else {
+                this.identity();
+            }
+        },
+        
+
+        /**
+         * セッター
+         */
+        set: function(m00, m01, m02, m10, m11, m12, m20, m21, m22)
+        {
+            console.assert(arguments.length>=9, "");
+            
+            // |m00, m10, m20|
+            // |m01, m11, m21|
+            // |m02, m12, m22|
+            
+            this.m[0]  = arguments[0];  // m00
+            this.m[1]  = arguments[1];  // m01
+            this.m[2]  = arguments[2];  // m02
+            this.m[3]  = arguments[3];  // m10
+            this.m[4]  = arguments[4];  // m11
+            this.m[5]  = arguments[5];  // m12
+            this.m[6]  = arguments[6];  // m20
+            this.m[7]  = arguments[7];  // m21
+            this.m[8]  = arguments[8];  // m22
+            
+            return this;
+        },
+        
+        /**
+         * 単位行列
+         */
+        identity: function() {
+            this.set(
+                1, 0, 0,
+                0, 1, 0,
+                0, 0, 1
+            );
+            return this;
+        },
+        
+        /**
+         * 文字列化
+         */
+        toString: function() {
+            return "|{m00}, {m10}, {m20}|\n|{m01}, {m11}, {m21}|\n|{m02}, {m12}, {m22}|".format(this);
+        },
+        
+    });
+    
+    
+    
+    /**
+     * @property    m00
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m00", {
+        "get": function()   { return this.m[0]; },
+        "set": function(v)  { this.m[0] = v;    }
+    });
+    /**
+     * @property    m01
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m01", {
+        "get": function()   { return this.m[1]; },
+        "set": function(v)  { this.m[1] = v;    }
+    });
+    /**
+     * @property    m02
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m02", {
+        "get": function()   { return this.m[2]; },
+        "set": function(v)  { this.m[2] = v;    }
+    });
+    
+    /**
+     * @property    m10
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m10", {
+        "get": function()   { return this.m[3]; },
+        "set": function(v)  { this.m[3] = v;    }
+    });
+    /**
+     * @property    m11
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m11", {
+        "get": function()   { return this.m[4]; },
+        "set": function(v)  { this.m[4] = v;    }
+    });
+    /**
+     * @property    m12
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m12", {
+        "get": function()   { return this.m[5]; },
+        "set": function(v)  { this.m[5] = v;    }
+    });
+    
+    /**
+     * @property    m20
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m20", {
+        "get": function()   { return this.m[6]; },
+        "set": function(v)  { this.m[6] = v;    }
+    });
+    /**
+     * @property    m21
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m21", {
+        "get": function()   { return this.m[7]; },
+        "set": function(v)  { this.m[7] = v;    }
+    });
+    /**
+     * @property    m22
+     * 要素
+     */
+    tm.geom.Matrix33.prototype.accessor("m22", {
+        "get": function()   { return this.m[8]; },
+        "set": function(v)  { this.m[8] = v;    }
+    });
+    
+})();
