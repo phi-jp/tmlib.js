2479aee24ad60a370e1ce38ab022564e62734206
phi <phi1618jp@gmail.com>
2012-04-25 00:11:47 +0900

    Math 拡張

--- src/core/math.js
+++ src/core/math.js
@@ -26,5 +26,45 @@
         return window.Math.random()*(max-min)+min;
     };
     
+    /**
+     * @method
+     * Degree を Radian に変換
+     */
+    Math.degToRad = function(deg) {
+        return deg / 180.0 * Math.PI;
+    };
+    
+    /**
+     * @method
+     * Radian を Degree に変換
+     */
+    Math.radToDeg = function(rad) {
+        return rad * 180.0 / Math.PI;
+    };
+    
+    
+    /**
+     * @method
+     * 長さを取得
+     */
+    Math.magnitude = function() {
+        return Math.sqrt(Math.magnitudeSq.apply(null, arguments));
+    };
+    
+    
+    /**
+     * @method
+     * 長さの２乗を取得
+     */
+    Math.magnitudeSq = function() {
+        var n = 0;
+        
+        for (var i=0,len=arguments.length; i<len; ++i) {
+            n += arguments[i]*arguments[i];
+        }
+        
+        return n;
+    };
+    
 })();
 
