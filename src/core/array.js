eb7948d28d9bfe29eaed467b58b24966b852c91b
phi <phi1618jp@gmail.com>
2012-04-28 02:08:14 +0900

    配列拡張を移植

--- src/core/array.js
+++ src/core/array.js
@@ -2,3 +2,143 @@
  * array.js
  */
 
+
+(function() {
+    
+    /**
+     * @class   Array
+     * 配列
+     */
+    
+    /**
+     * @method  swap
+     * a番目 と b番目 を入れ替える
+     */
+    Array.defineInstanceMethod("swap", function(a, b) {
+        var temp = this[a];
+        this[a] = this[b];
+        this[b] = temp;
+        return this;
+    });
+    
+    
+    /**
+     * elm と一致する要素を削除
+     */
+    Array.defineInstanceMethod("erase", function(elm) {
+        var index  = this.indexOf(elm);
+        this.splice(index, 1);
+        return this;
+    });
+    
+    /**
+     * elm と一致する要素を全て削除
+     */
+    Array.defineInstanceMethod("eraseAll", function(elm) {
+        for (var i=0,len=this.length; i<len; ++i) {
+            if (this[i] == elm) {
+                this.splice(i--, 1);
+            }
+        }
+        return this;
+    });
+    
+    /**
+     * 条件にマッチした要素を削除
+     */
+    Array.defineInstanceMethod("eraseIf", function(fn) {
+        for (var i=0,len=this.length; i<len; ++i) {
+            if ( fn(this[i], i, this) ) { this.splice(i--, 1); }
+        }
+        return this;
+    });
+    
+    /**
+     * 要素の中からランダムで取り出す
+     */
+    Array.defineInstanceMethod("random", function(min, max) {
+        min = min || 0;
+        max = max || this.length;
+        return this[ Math.rand(min, max) ];
+    });
+    
+    
+    /**
+     * 重複削除
+     */
+    Array.defineInstanceMethod("uniq", function(deep) {
+        var arr = [];
+        for (var i=0,len=this.length; i<len; ++i) {
+            var value = this[i];
+            if (value in arr == false) {
+                arr.push(value);
+            }
+        }
+        return arr;
+    });
+    
+    /**
+     * フラット.
+     * Ruby のやつ.
+     */
+    Array.defineInstanceMethod("flatten", function(deep) {
+        var arr = [];
+        for (var i=0,len=this.length; i<len; ++i) {
+            var value = this[i];
+            if (value instanceof Array) {
+                arr = arr.concat(value.flatten());
+            }
+            else {
+                arr.push(value);
+            }
+        }
+        return arr;
+    });
+    
+    /**
+     * 配列をクローン
+     */
+    Array.defineInstanceMethod("clone", function(deep) {
+        if (deep == true) {
+            var a = Array(this.length);
+            for (var i=0,len=this.length; i<len; ++i) {
+                a[i] = (this[i].clone) ? this[i].clone(deep) : this[i];
+            }
+            return a;
+        };
+        
+        return Array.prototype.slice.apply(this);
+    });
+    
+    /**
+     * クリア
+     */
+    Array.defineInstanceMethod("clear", function() {
+        this.length = 0;
+        return this;
+    });
+    
+    /**
+     * 特定の値で満たす
+     */
+    Array.defineInstanceMethod("fill", function() {
+        // TODO:
+        return this;
+    });
+    
+    /**
+     * 
+     */
+    Array.defineInstanceMethod("toULElement", function(){
+        // TODO: 
+    });
+
+    /**
+     * 
+     */
+    Array.defineInstanceMethod("toLIElement", function(){
+        // TODO:
+    });
+    
+})();
+
