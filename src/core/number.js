0bbe448f8bdb561939828f6964236d08966a116e
phi <phi1618jp@gmail.com>
2012-04-28 21:44:29 +0900

    Number 拡張を移植 テストページを追加

--- src/core/number.js
+++ src/core/number.js
@@ -1,3 +1,99 @@
 /*
  * number.js
  */
+
+
+(function() {
+    
+    /**
+     * @class   Number
+     * 数値
+     */
+    
+    /**
+     * @method  round
+     * 四捨五入
+     * 桁数指定版
+     */
+    Number.defineInstanceMethod("round", function(figure) {
+        figure = figure || 0;
+        var base = Math.pow(10, figure);
+        var temp = this * base;
+        temp = Math.round(temp);
+        return temp/base;
+    });
+    
+    /**
+     * @method  ceil
+     * 切り上げ.
+     * 桁数指定版
+     */
+    Number.defineInstanceMethod("ceil",  function(figure) {
+        figure = figure || 0;
+        var base = Math.pow(10, figure);
+        var temp = this * base;
+        temp = Math.ceil(temp);
+        return temp/base;
+    });
+    /**
+     * @method  floor
+     * 切り捨て
+     * 桁数指定版
+     */
+    Number.defineInstanceMethod("floor",  function(figure) {
+        figure = figure || 0;
+        var base = Math.pow(10, figure);
+        var temp = this * base;
+        temp = Math.floor(temp);
+        return temp/base;
+    });
+    
+    /**
+     * @method  toInt
+     * integer 型に変換する
+     */
+    Number.defineInstanceMethod("toInt",  function() {
+        return (this | 0);
+    });
+    
+    /**
+     * @method  toUnsigned
+     * unsigned 型に変換する
+     */
+    Number.defineInstanceMethod("toUnsigned",  function() {
+        return this >>> 0;
+    });
+    
+    /**
+     * @method  toHex
+     * 16進数化
+     */
+    Number.defineInstanceMethod("toHex",  function() {
+        return this.toString(16);
+    });
+    
+    /**
+     * @method  toBin
+     * 2進数化
+     */
+    Number.defineInstanceMethod("toBin",  function() {
+        return this.toString(2);
+    });
+    
+    
+    /**
+     * @method  padding
+     * 文字埋め
+     */
+    Number.defineInstanceMethod("padding",  function(n, ch) {
+        var str = this+'';
+        n  = n-str.length;
+        ch = ch || '0';
+        
+        while(n-- > 0) { str = ch + str; }
+        
+        return str;
+    });
+    
+})();
+
