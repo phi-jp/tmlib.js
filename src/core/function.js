f8df2a961cc65ce0586cda1a9f8be27c6d67df5a
phi <phi1618jp@gmail.com>
2012-04-28 21:48:43 +0900

    function 拡張を移植, ビルド

--- src/core/function.js
+++ src/core/function.js
@@ -1,3 +1,39 @@
 /*
  * function.js
  */
+
+
+(function() {
+    
+    /**
+     * @class   Function
+     * 関数
+     */
+    
+    /**
+     * @method  toArrayFunction
+     * 関数を配列対応関数に変換.
+     * forEach の逆アプローチ的な感じ.
+     * 配列を継承したクラスなどに使用する.
+     * ## Example
+     *      var hoge = function(n) { console.log(this*n); return this*n; };
+     *      var arr = [5, 10, 15];
+     *      arr.hogeArray = hoge.toArrayFunction();
+     *      var result = arr.hogeArray(100);
+     *      console.log(result);
+     */
+    Function.defineInstanceMethod("toArrayFunction", function() {
+        var self = this;
+        return function() {
+            var resultList = [];
+            for (var i=0,len=this.length; i<len; ++i) {
+                resultList.push( self.apply(this[i], arguments) );
+            }
+            return resultList;
+        }
+    });
+    
+    // forEach や map はもう標準化されてきてるので実装しないよん♪
+    
+});
+
