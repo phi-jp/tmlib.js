70141db41e78063d796d961827dcda640f56cd5d
phi <phi1618jp@gmail.com>
2012-04-30 03:28:47 +0900

    tm.graphics.Texture, tm.graphics.TextureManager クラスを追加

---
+++ src/graphics/texture.js
@@ -0,0 +1,71 @@
+/*
+ * texture.js
+ */
+
+tm.graphics = tm.graphics || {};
+
+(function() {
+    
+    /**
+     * @class
+     * テクスチャクラス
+     */
+    tm.graphics.Texture = tm.createClass({
+        
+        element: null,
+        loaded: false,
+        
+        /**
+         * 初期化
+         */
+        init: function(src) {
+            this.element = new Image();
+            this.element.src = src;
+            
+            var self = this;
+            this.element.onload = function() {
+                self.loaded = true;
+            };
+        }
+        
+    });
+    
+})();
+
+(function(){
+    
+    /**
+     * @class
+     * テクスチャマネージャクラス
+     */
+    tm.graphics.TextureManager = {
+        textures: {},
+        loaded: true,
+    };
+    
+    /**
+     * @static
+     * @method
+     * 追加
+     */
+    tm.graphics.TextureManager.add = function(name, src)
+    {
+        if (src === undefined) { src = name; }
+        
+        this.textures[name] = tm.graphics.Texture(src);
+        this.loaded = false;
+    };
+    
+    /**
+     * @static
+     * @method
+     * 取得
+     */
+    tm.graphics.TextureManager.get = function(name)
+    {
+        return this.textures[name];
+    };
+    
+})();
+
+
