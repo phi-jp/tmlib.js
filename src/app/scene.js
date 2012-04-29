d66289cc1fc91c0508bcd0e29eaab1056015fbf0
phi <phi1618jp@gmail.com>
2012-04-30 03:28:18 +0900

    text の幅を設定

--- src/app/scene.js
+++ src/app/scene.js
@@ -38,10 +38,11 @@ tm.app = tm.app || {};
             var label = tm.app.Label("Start");
             label.x = e.app.canvas.width/2;
             label.y = e.app.canvas.height/2;
+            label.width = e.app.canvas.width;
             label.align     = "center";
             label.baseline  = "middle";
             this.addChild(label);
-            console.log("start");
+            
             // タッチに反応させる
             this.width  = e.app.canvas.width;
             this.height = e.app.canvas.height;
@@ -68,10 +69,10 @@ tm.app = tm.app || {};
             var label = tm.app.Label("end");
             label.x = e.app.canvas.width/2;
             label.y = e.app.canvas.height/2;
+            label.width = e.app.canvas.width;
             label.align     = "center";
             label.baseline  = "middle";
             this.addChild(label);
-            console.log("end");
             // タッチに反応させる
             this.width  = e.app.canvas.width;
             this.height = e.app.canvas.height;
