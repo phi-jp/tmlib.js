7386f78676edf122143fa4d25a4f7fe6bfa54920
phi <phi1618jp@gmail.com>
2012-05-07 22:09:15 +0900

    gradient を追加

---
+++ src/graphics/gradient.js
@@ -0,0 +1,106 @@
+/*
+ * gradient.js
+ */
+
+tm.graphics = tm.graphics || {};
+
+(function() {
+    
+    tm.graphics.Canvas.prototype.setGradient = function(gradient) {
+        this.context.fillStyle = gradient.gradient;
+    };
+    
+})();
+
+(function() {
+    
+    /**
+     * @class
+     * 線形グラデーション
+     */
+    tm.graphics.LinearGradient = tm.createClass({
+        
+        init: function(x, y, width, height) {
+            this.gradient = dummyContext.createLinearGradient(x, y, width, height);
+        },
+        
+        addColorStop: function(offset, color) {
+            this.gradient.addColorStop(offset, color);
+            return this;
+        },
+        
+        addColorStopList: function(prop) {
+            for (var i=0,len=prop.length; i<len; ++i) {
+                var offset  = prop[i].offset;
+                var color   = prop[i].color;
+                this.addColorStop(offset, color);
+            }
+            return this;
+        },
+        
+    });
+    
+    
+    var dummyCanvas = document.createElement("canvas");
+    var dummyContext= dummyCanvas.getContext("2d");
+    
+    
+})();
+
+
+(function() {
+    
+    /**
+     * @class
+     * 円形グラデーション
+     */
+    tm.graphics.RadialGradient = tm.createClass({
+        
+        init: function(x0, y0, r0, x1, y1, r1) {
+            this.gradient = dummyContext.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        },
+        
+        addColorStop: function(offset, color) {
+            this.gradient.addColorStop(offset, color);
+            return this;
+        },
+        
+        addColorStopList: function(prop) {
+            for (var i=0,len=prop.length; i<len; ++i) {
+                var offset  = prop[i].offset;
+                var color   = prop[i].color;
+                this.addColorStop(offset, color);
+            }
+            return this;
+        },
+        
+    });
+    
+    
+    var dummyCanvas = document.createElement("canvas");
+    var dummyContext= dummyCanvas.getContext("2d");
+    
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
