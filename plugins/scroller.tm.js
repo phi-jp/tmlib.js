36c59a3b0eab0881b41e27baa535c6ae1cb1a29e
phi <phi1618jp@gmail.com>
2012-06-04 22:22:27 +0900

    torne interface サンプルを移植

---
+++ plugins/scroller.tm.js
@@ -0,0 +1,20 @@
+/*
+ * scroller.tm.js
+ */
+
+
+(function() {
+    
+    /**
+     * @class
+     * スクローラクラス
+     */
+    tm.Scroller = tm.createClass({
+        
+        init: function(arg) {
+        },
+        
+    });
+    
+})();
+
