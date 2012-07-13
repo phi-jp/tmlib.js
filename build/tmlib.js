820a5a2470ee9cecce245907f9a308bf1bdb7f34
phi <phi1618jp@gmail.com>
2012-07-14 04:14:48 +0900

    add chart api.

--- build/tmlib.js
+++ build/tmlib.js
@@ -10706,11 +10706,30 @@ tm.google = tm.google || {};
      */
     tm.google.Chart = tm.google.Chart || {};
     
-    var BASE_URL = "https://chart.googleapis.com/chart?chs={size}&cht={type}&chl={text}";
-    
+    var DYNAMIC_ICONS_BASE_URL  = "https://chart.googleapis.com/chart?chst={type}&chld={data}";
+    var QR_CODE_BASE_URL        = "https://chart.googleapis.com/chart?chs={size}&cht={type}&chl={text}&chco={color}";
     
     /**
-     * Tweet する
+     * ダイナミックアイコン
+     * @param {string}  type    d_bubble_text_small OR d_fnote_title OR chst=d_fnote etc...
+     * @param {string}  data    
+     * ### Example
+     *      tm.google.Chart.createDynamicIcons("d_bubble_icon_text_small", "ski|bb|Hello, world!|FFFFFF|000000");
+     *      tm.google.Chart.createDynamicIcons("d_fnote", "balloon|1|000000|l|Hello, world!");
+     *      tm.google.Chart.createDynamicIcons("d_fnote", "pinned_c|1|000000|l|Hello, world!");
+     *      tm.google.Chart.createDynamicIcons("d_weather", "taped_y|sunny|Barcelona|max+25°C|min+15°C");
+     *      tm.google.Chart.createDynamicIcons("d_simple_text_icon_left", "flag_jp|14|000|flag_jp|24|000|FFF");
+     */
+    tm.google.Chart.createDynamicIcons = function(type, data) {
+        // data = encodeURIComponent(data);
+        return DYNAMIC_ICONS_BASE_URL.format({
+            type:type,
+            data:data
+        });
+    };
+    
+    /**
+     * QRCode 生成
      * @param {Object} prop
      * ### Reference
      * - <https://developers.google.com/chart/?hl=ja#qrcodes>
@@ -10720,14 +10739,21 @@ tm.google = tm.google || {};
      *      tm.google.Chart.createQRCode("160x160", "http://tmlife.net");
      *      tm.google.Chart.createQRCode("160x160", "Hello, world");
      */
-    tm.google.Chart.createQRCode = function(size, text) {
+    tm.google.Chart.createQRCode = function(size, text, color) {
         text = encodeURIComponent(text);
         
-        return BASE_URL.format({
+        return QR_CODE_BASE_URL.format({
             type:"qr",
             size:size,
             text:text,
         });
+    };
+    
+    /**
+     * 
+     */
+    tm.google.Chart.createTeX = function() {
+        //&chco=ff0000ff
     };
     
 })();
