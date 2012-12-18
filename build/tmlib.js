87975499daf6d306a09d1aa282af801c23d76b15
phi <phi1618jp@gmail.com>
2012-12-19 00:22:43 +0900

    build

--- build/tmlib.js
+++ build/tmlib.js
@@ -9378,6 +9378,8 @@ tm.app = tm.app || {};
         },
 
         gotoAndPlay: function(name) {
+            name = name || "default";
+            
             this.paused = false;
             this.currentAnimation = this.ss.animations[name];
             this.currentFrame = 0;
@@ -9386,6 +9388,8 @@ tm.app = tm.app || {};
         },
 
         gotoAndStop: function(name) {
+            name = name || "default";
+            
             this.paused = true;
             this.currentAnimation = this.ss.animations[name];
             this.currentFrame = 0;
@@ -9439,13 +9443,20 @@ tm.app = tm.app || {};
         getFrame: function(index) {
             return this.frames[index];
         },
-
+        
+        getAnimation: function(name) {
+            return this.animations[name];
+        },
+        
         _calcFrames: function(frame) {
             var frames = this.frames = [];
             
             var w = frame.width;
             var h = frame.height;
             var row = ~~(this.image.width / w);
+            var col = ~~(this.image.height/ h);
+            
+            if (!frame.count) frame.count = row*col;
 
             for (var i=0,len=frame.count; i<len; ++i) {
                 var x   = i%row;
@@ -9480,6 +9491,13 @@ tm.app = tm.app || {};
                     };
                 }
             }
+            
+            // デフォルトアニメーション
+            this.animations["default"] = {
+                frames: [].range(0, this.frame.count),
+                next: "default",
+                frequency: 1,
+            };
         }
 
     });
