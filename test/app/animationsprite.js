3f5a8a12f4327dfbf8a03e640aa62f69dc66b8d2
phi <phi1618jp@gmail.com>
2013-04-05 01:23:07 +0900

    fix

---
+++ test/app/animationsprite.js
@@ -0,0 +1,34 @@
+
+tm.preload(function() {
+    tm.graphics.TextureManager.add("crash", "../../resource/img/crash/01.png");
+    PLAYER_SPRITE_SHEET = tm.app.SpriteSheet({
+        image: "crash",
+        frame: {
+            width: 64,
+            height: 64,
+            count: 64
+        },
+        animations: {
+            "crash": [0, 32, "crash"],
+            "hoge": [32, 0, "crash", 5],
+        }
+    });
+});
+
+tm.define("tests.animationsprite.DemoScene", {
+    superClass: "tm.app.Scene",
+
+    init: function() {
+        this.superInit();
+    },
+
+    onpointingstart: function(e) {
+    	var p = e.app.pointing;
+        var crash = tm.app.AnimationSprite(128, 128, PLAYER_SPRITE_SHEET);
+        crash.position.set(p.x, p.y);
+        crash.gotoAndPlay("crash");
+        //crash.gotoAndStop("hoge");
+        crash.blendMode = "lighter";
+        this.addChild(crash);
+    }
+});
