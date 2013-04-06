5ce04beae948d9ced870c0efa97cbfd5a495433a
phi <phi1618jp@gmail.com>
2013-04-06 22:22:20 +0900

    fix

--- test/app/timeline.js
+++ test/app/timeline.js
@@ -2,36 +2,48 @@ tm.namespace("tm.app", function() {
     tm.define("tm.app.Timeline", {
         superClass: "tm.event.EventDispatcher",
         
+        /**
+         * 初期化
+         */
         init: function(elm) {
             this.superInit();
             
             this.setTarget(elm);
             
+            this.currentFrame = 0;
+            this.duration = 0;
+            this.isPlay = true;
             this._tweens  = [];
             this._actions = [];
-            this.currentTime = 0;
-            this.isPlay = true;
-            
-            this.stepFrame = 1000/30;
         },
         
+        /**
+         * 更新
+         */
         update: function(app) {
             if (!this.isPlay) return ;
             
-            this.currentTime += this.stepFrame;
-            this._update();
+            if (this.currentFrame > this.duration) {
+//                this.gotoAndPlay(0);
+            }
+            else {
+                this._updateTween();
+                this._updateAction();
+            }
+            
+            this.currentFrame++;
         },
         
-        _update: function() {
+        _updateTween: function() {
             var tweens = this._tweens;
             for (var i=0,len=tweens.length; i<len; ++i) {
                 var tween = tweens[i];
                 
-                if (tween.delay > this.currentTime) {
+                if (tween.delay > this.currentFrame) {
                     continue ;
                 }
                 
-                var time = this.currentTime - tween.delay;
+                var time = this.currentFrame - tween.delay;
                 tween._setTime(time);
                 if (tween.time >= tween.duration) {
                 }
@@ -39,23 +51,20 @@ tm.namespace("tm.app", function() {
                     tween.update();
                 }
             }
-            
-            this._updateAction(this.currentTime-this.stepFrame|0, this.currentTime);
         },
         
-        _updateAction: function(startTime, endTime) {
+        _updateAction: function() {
             var actions = this._actions;
             
             for (var i=0,len=actions.length; i<len; ++i) {
                 var action = actions[i];
                 
-                if (startTime <= action.data.delay && action.data.delay <= endTime) {
+                if (action.delay == this.currentFrame) {
                     if (action.type == "call") {
-                        action.data.func();
+                        action.func();
                     }
                     else if (action.type == "set") {
-                        var props = action.data.props;
-                        console.log(props);
+                        var props = action.props;
                         for (var key in props) {
                             this.element[key] = props[key];
                         }
@@ -64,6 +73,9 @@ tm.namespace("tm.app", function() {
             }
         },
         
+        /**
+         * アニメーション
+         */
         to: function(props, duration, delay, fn) {
             delay = delay || 0;
             var tween = tm.anim.Tween();
@@ -72,34 +84,43 @@ tm.namespace("tm.app", function() {
             
             this._tweens.push(tween);
             
+            this._updateDuration(tween);
+            
             return this;
         },
         
+        /**
+         * 関数を実行
+         */
         call: function(func, delay) {
             delay = delay || 0;
             
             this._actions.push({
                 "type": "call",
-                "data": {
-                    func: func,
-                    delay: delay,
-                }
+                func: func,
+                delay: delay,
             });
+            this._updateDuration(this._actions.last);
         },
         
+        /**
+         * プロパティをセット
+         */
         set: function(props, delay) {
             delay = delay || 0;
             
             this._actions.push({
                 "type": "set",
-                "data": {
-                    props: props,
-                    delay: delay,
-                }
+                props: props,
+                delay: delay,
             });
+            this._updateDuration(this._actions.last);
             return this;
         },
         
+        /**
+         * ターゲットをセット
+         */
         setTarget: function(target) {
             if (this._fn) {
                 this.element.removeEventListener("enterframe", this._fn);
@@ -109,22 +130,42 @@ tm.namespace("tm.app", function() {
             this._fn = function(e) { this.update(e.app); }.bind(this);
             this.element.addEventListener("enterframe", this._fn);
         },
+        
+        /**
+         * ターゲットをゲット
+         */
         getTarget: function(target) {
             return this.element;
         },
         
-        gotoAndPlay: function(time) {
+        /**
+         * ターゲットをゲット
+         */
+        gotoAndPlay: function(frame) {
             this.isPlay = true;
-            this.currentTime = time;
-            this._update();
+            this.currentFrame = frame;
+            this._updateTween();
         },
         
-        gotoAndStop: function(time) {
-            this.currentTime = time;
+        gotoAndStop: function(frame) {
+            this.currentFrame = frame;
             this.isPlay = false;
-            this._update();
+            this._updateTween();
         },
         
+        _updateDuration: function(task) {
+            var duration = task.delay + (task.duration ? task.duration : 0);
+            if (this.duration < duration) this.duration = duration;
+            return this;
+        },
+        
+        clear: function() {
+            this.currentFrame = 0;
+            this.duration = 0;
+            this.isPlay = true;
+            this._tweens  = [];
+            this._actions = [];
+        }
         
     });
     
