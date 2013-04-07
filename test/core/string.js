fc52ed7cb2e6b58ed090bc6c2a26ac20b31e6235
phi <phi1618jp@gmail.com>
2013-04-07 21:39:38 +0900

    fix

--- test/core/string.js
+++ test/core/string.js
@@ -41,5 +41,9 @@ describe('String Test', function() {
     it('paddingRight', function() {
         assert.equal("1234".paddingRight(10, '0'), '1234000000');
     });
+
+    it('repeat', function() {
+        assert.equal("1234".repeat(4), '1234123412341234');
+    });
     
 });
