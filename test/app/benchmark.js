627998adc0881b6ba4da62b7bae2f1169ebe79f2
phi <phi1618jp@gmail.com>
2013-04-15 00:09:55 +0900

    add crash benchmark test

--- test/app/benchmark.js
+++ test/app/benchmark.js
@@ -1,9 +1,9 @@
 tm.preload(function() {
-	tm.graphics.TextureManager.add("piyo", "piyo.png");
-	tm.graphics.TextureManager.add("9iIpS", "http://jsrun.it/assets/9/i/I/p/9iIpS.png");
+    tm.graphics.TextureManager.add("piyo", "piyo.png");
+    tm.graphics.TextureManager.add("9iIpS", "http://jsrun.it/assets/9/i/I/p/9iIpS.png");
 
-	tm.util.ScriptManager.loadStats();
-	tm.util.ScriptManager.loadDatGUI();
+    tm.util.ScriptManager.loadStats();
+    tm.util.ScriptManager.loadDatGUI();
 });
 
 tm.define("tests.benchmark.PiyoScene", {
@@ -24,13 +24,13 @@ tm.define("tests.benchmark.PiyoScene", {
     },
 
     onpointingstart: function() {
-    	this._resetObject();
-    	app.stop();
+        this._resetObject();
+        app.stop();
     },
  
     update: function(app) {
-    	var objList = this.objList;
-    	var len = objList.length;
+        var objList = this.objList;
+        var len = objList.length;
         for (var i = 0; i < len; i++) {
             var container = objList[i];
             container.y += container.vy;
@@ -39,7 +39,7 @@ tm.define("tests.benchmark.PiyoScene", {
         }
     },
     _resetObject: function() {
-    	var objList = this.objList;
+        var objList = this.objList;
         for (var i = 0; i < objList.length; i++) {
             var container = objList[i];
             app.currentScene.removeChild(container);
@@ -47,7 +47,7 @@ tm.define("tests.benchmark.PiyoScene", {
     },
 
     _generateObject: function() {
-    	this._resetObject();
+        this._resetObject();
 
         this.objList = [];
 
@@ -57,7 +57,7 @@ tm.define("tests.benchmark.PiyoScene", {
         var CHILD_RADIUS = 50;
 
         var updateFunc = function() {
-        	this.rotation -= 10;
+            this.rotation -= 10;
         };
 
         for (var i = 0; i < maxCount; i++) {
@@ -80,16 +80,16 @@ tm.define("tests.benchmark.PiyoScene", {
 
                 child.update = updateFunc;
                 // child.hoge = function() {
-                // 	this.rotation -= 10;
+                //  this.rotation -= 10;
                 // };
                 // child.draw = function(canvas) {
-                // 	canvas.fillCircle(0, 0, 10);
+                //  canvas.fillCircle(0, 0, 10);
                 // };
                 container.addChild(child);
             }
         }
 
-//        textNumObj.innerHTML = String(maxCount * CHILD_NUM);    	
+//        textNumObj.innerHTML = String(maxCount * CHILD_NUM);      
     },
 });
 
@@ -115,47 +115,47 @@ tm.define("tests.benchmark.HogeScene", {
 
         var gui = app.enableDatGUI();
         gui.add(this, "TOTAL", 500, 10000).step(500).name('Particles').onFinishChange(function() {
-        	this._addParticle();
+            this._addParticle();
         }.bind(this));
         gui.domElement.parentNode.style.zIndex = 20000;
     },
 
     update: function(app) {
-    	var p = app.pointing;
-    	var len = this.particles.length;
-	    for (var i = 0; i < len; i++) {
-			var ball = this.particles[i].sprite;
-			var dx = ball.position.x - p.x;
-			var dy = ball.position.y - p.y;
-			var vx = this.particles[i].vx;
-			var vy = this.particles[i].vy;     
-	       
-			if (dx * dx + dy * dy <= 10000) {
-				vx += dx * 0.01;
-				vy += dy * 0.01;
-			}
-			vx *= 0.95;
-			vy *= 0.95;
+        var p = app.pointing;
+        var len = this.particles.length;
+        for (var i = 0; i < len; i++) {
+            var ball = this.particles[i].sprite;
+            var dx = ball.position.x - p.x;
+            var dy = ball.position.y - p.y;
+            var vx = this.particles[i].vx;
+            var vy = this.particles[i].vy;     
+           
+            if (dx * dx + dy * dy <= 10000) {
+                vx += dx * 0.01;
+                vy += dy * 0.01;
+            }
+            vx *= 0.95;
+            vy *= 0.95;
 
-			vx += Math.random() - 0.5;
-			vy += Math.random() - 0.5;
+            vx += Math.random() - 0.5;
+            vy += Math.random() - 0.5;
 
-			var x = ball.position.x += vx;
-			var y = ball.position.y += vy;
+            var x = ball.position.x += vx;
+            var y = ball.position.y += vy;
 
-			if (x < 0 || x > app.width || y < 0 || y > app.height) {
-				var r = Math.atan2(y - app.height / 2, x - app.width / 2);
-				vx = -Math.cos(r);
-				vy = -Math.sin(r);
-			}
+            if (x < 0 || x > app.width || y < 0 || y > app.height) {
+                var r = Math.atan2(y - app.height / 2, x - app.width / 2);
+                vx = -Math.cos(r);
+                vy = -Math.sin(r);
+            }
 
-			this.particles[i].vx = vx;
-			this.particles[i].vy = vy;      
-		}
+            this.particles[i].vx = vx;
+            this.particles[i].vy = vy;      
+        }
     },
 
     _addParticle: function() {
-    	this._removeAllBall();
+        this._removeAllBall();
 
         for (i = 0; i < this.TOTAL; i++) {  
             var ball = tm.app.Sprite(29, 29, "9iIpS");
@@ -183,3 +183,30 @@ tm.define("tests.benchmark.HogeScene", {
 });
 
 
+tm.define("tests.benchmark.CrashScene", {
+    superClass: "tm.app.Scene",
+
+    init: function() {
+        this.superInit();
+
+        app.fps = 60;
+        app.enableStats();
+        app.stats.domElement.style.zIndex = 1100;
+
+        for (var i=0; i<256; ++i) {
+            var crash = tm.app.AnimationSprite(128, 128, PLAYER_SPRITE_SHEET).addChildTo(this);
+            var x = tm.util.Random.randint(0, SCREEN_WIDTH);
+            var y = tm.util.Random.randint(0, SCREEN_HEIGHT);
+            crash.position.set(x, y);
+            crash.blendMode = "lighter";
+            crash.timeline.call(function() {
+                this.gotoAndPlay("crash");
+            }.bind(crash), i*50);
+        }
+    },
+
+    onpointingstart: function(e) {
+        var p = e.app.pointing;
+
+    }
+});
