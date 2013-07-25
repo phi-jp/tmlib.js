2103298542c9927e171eb15288387cee7d915578
phi <phi1618jp@gmail.com>
2013-07-25 15:00:10 +0900

    adjust test

---
+++ test/app/main.js
@@ -0,0 +1,16 @@
+
+;(function(global) {
+    testhelper.loadtmlibAll("../../");
+    
+    testhelper.loadScripts([
+        "scripts/object2d.js",
+        "scripts/interaction.js",
+        "scripts/collision.js",
+        "scripts/tweener.js",
+        "scripts/timeline.js",
+        
+        "scripts/dialog.js",
+        "scripts/button.js",
+    ]);
+})(this);
+
---
+++ test/display/main.js
@@ -0,0 +1,12 @@
+
+;(function(global) {
+    testhelper.loadtmlibAll("../../");
+    
+    testhelper.loadScripts([
+        "scripts/mapsprite.js",
+        "scripts/animationsprite.js",
+        "scripts/canvaselement.js",
+        "scripts/mapsprite.js",
+    ]);
+})(this);
+
