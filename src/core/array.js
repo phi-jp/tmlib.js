a7dec37b22bfc384ef5b5cfdc23f9c71b6db5904
phi <phi1618jp@gmail.com>
2014-02-10 14:09:25 +0900

    bugfix

--- src/core/array.js
+++ src/core/array.js
@@ -170,7 +170,7 @@
         var arr = [];
         for (var i=0,len=this.length; i<len; ++i) {
             var value = this[i];
-            if (this.indexOf(value) == -1) {
+            if (arr.indexOf(value) == -1) {
                 arr.push(value);
             }
         }
