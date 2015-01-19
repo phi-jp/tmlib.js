151bbb9786fe549b4920fb34672f2123a243d43f
phi <phi1618jp@gmail.com>
2015-01-19 15:12:53 +0900

    build

--- build/tmlib.js
+++ build/tmlib.js
@@ -6873,7 +6873,7 @@ tm.dom = tm.dom || {};
             // type が省略されている場合は拡張子から判定する
             type = type || path.split('?')[0].split('#')[0].split('.').last;
             
-            var asset = tm.asset.Loader._funcs[type](path);
+            var asset = tm.asset.Loader._funcs[type](path, key);
             this.set(key, asset);
             
             return asset;
@@ -7549,6 +7549,59 @@ tm.dom = tm.dom || {};
     });
 
 })();
+tm.asset = tm.asset || {};
+
+(function() {
+
+    tm.define("tm.asset.Font", {
+        superClass: "tm.event.EventDispatcher",
+
+        init: function(path, key) {
+            this.superInit();
+
+            var fontFaceStyleElement = tm.dom.Element("head").create("style");
+            fontFaceStyleElement.text = "@font-face { font-family: '{0}'; src: url({1}) format('truetype'); }".format(key, path);
+
+            tm.asset.Font.checkLoaded(key, function() {
+                this.flare("load");
+            }.bind(this));
+        },
+    });
+
+    tm.asset.Font.checkLoaded = function(font, callback) {
+        var element = tm.dom.Element("body").create("span");
+        element.style
+            .set("color", "rgba(0, 0, 0, 0)")
+            .set("fontSize", "40px");
+        element.text = "QW@HhsXJ=/()あいうえお＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝";
+
+        var before = element.element.offsetWidth;
+        element.style
+            .set("fontFamily", "'{0}', 'monospace'".format(font));
+
+        var checkLoadFont = function() {
+            if (element.element.offsetWidth !== before) {
+                element.remove();
+                callback && callback();
+            } else {
+                setTimeout(checkLoadFont, 100);
+            }
+        };
+        setTimeout(checkLoadFont, 100);
+    };
+
+    tm.asset.Loader.register("ttf", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+    tm.asset.Loader.register("otf", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+    tm.asset.Loader.register("woff", function(path, key) {
+        return tm.asset.Font(path, key);
+    });
+
+})();
+
 /*
  * keyboard.js
  */
