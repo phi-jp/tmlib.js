ab7cd870a293e32d3344e7f1f6d378dace12d648
phi <phi1618jp@gmail.com>
2015-01-19 16:32:47 +0900

    add grid

---
+++ src/app/grid.js
@@ -0,0 +1,31 @@
+
+
+
+;(function() {
+    
+    /**
+     * @class tm.app.Grid
+     * @extends tm.app.Object2d
+     * グリッド
+     */
+    tm.define("tm.display.CircleShape", {
+        superClass: tm.app.Object2d,
+
+        cellWidth: 64,
+        cellHeight: 64,
+        
+        /**
+         * @constructor
+         */
+        init: function(param) {
+            this.superInit(param);
+
+        },
+
+        reposition: function() {
+        	// TODO: 
+        },
+    });
+    
+})();
+
