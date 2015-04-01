c6d2555fc1d49b491f4fefb1a543cf64fc0845d2
phi <phi1618jp@gmail.com>
2015-04-01 20:41:48 +0900

    build

--- build/tmlib.js
+++ build/tmlib.js
@@ -17271,6 +17271,15 @@ tm.ui = tm.ui || {};
 
         var scenes = [
             {
+                className: "SplashScene",
+                arguments: {
+                    width: param.width,
+                    height: param.height,
+                },
+                label: "splash",
+                nextLabel: "title",
+            },
+            {
                 className: "TitleScene",
                 arguments: {
                     title: param.title,
@@ -17882,6 +17891,53 @@ tm.ui = tm.ui || {};
 
 
 
+
+/*
+ * splash
+ */
+
+;(function() {
+    var SPLASH_IMAGE_PATH = "https://files.gitter.im/phi-jp/tmlib.js/t5F7/splash.png";
+
+    tm.define("SplashScene", {
+        superClass: "tm.app.Scene",
+
+        init: function(param) {
+            this.superInit();
+
+            this.param = param;
+
+            this.splashImage = tm.asset.Texture(param.path || SPLASH_IMAGE_PATH);
+            this.splashImage.onload = this._init.bind(this);
+        },
+
+        _init: function() {
+            var width = this.param.width;
+            var height = this.param.height;
+            
+            tm.display.Shape({
+                width: width,
+                height: height,
+                bgColor: "white"
+            }).setOrigin(0, 0).addChildTo(this);
+
+            tm.display.Sprite(this.splashImage, width, height)
+                .setOrigin(0, 0)
+                .setAlpha(0)
+                .addChildTo(this)
+                .tweener
+                    .clear()
+                    .wait(250)
+                    .fadeIn(500)
+                    .wait(1000)
+                    .fadeOut(500)
+                    .wait(250)
+                    .call(function() {
+                        this.app.popScene();
+                    }.bind(this));
+        },
+    });
+})();
 
 
 /*
