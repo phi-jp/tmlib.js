b079dd8dc5ecc5c866f3ec3da5f134fb9503da24
phi <phi1618jp@gmail.com>
2015-04-08 15:23:38 +0900

    add exit method in Scene class

--- src/app/scene.js
+++ src/app/scene.js
@@ -15,6 +15,8 @@ tm.app = tm.app || {};
     tm.app.Scene = tm.createClass({
         superClass: tm.app.Object2D,
 
+        app: null,
+
         /** ManagerScene 経由で生成された際に次にどのシーンに遷移するかのラベル */
         nextLabel: "",
 
@@ -31,6 +33,28 @@ tm.app = tm.app || {};
             
             // タッチに反応させる
             this.setInteractive(true);
+        },
+
+        exit: function(param) {
+            if (!this.app) return ;
+
+            if (typeof param !== 'object') {
+                var temp = {};
+                temp.nextLabel = arguments[0];
+                temp.nextArguments = arguments[1];
+                param = temp;
+            }
+
+            if (param.nextLabel) {
+                this.nextLabel = param.nextLabel;
+            }
+            if (param.nextArguments) {
+                this.nextArguments = param.nextArguments;
+            }
+
+            this.app.popScene();
+
+            return this;
         },
 
     });
