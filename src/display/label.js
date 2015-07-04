1a8f5909cad2b87b18aca5fdad518a3f1123cf18
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2015-07-05 07:14:20 +0900

    label auto new line.

--- src/display/label.js
+++ src/display/label.js
@@ -7,8 +7,8 @@ tm.display = tm.display || {};
 
 (function() {
     
-    var dummyCanvas  = null;
-    var dummyContext = null;
+    var dummyCanvas = document.createElement("canvas");
+    var dummyContext = dummyCanvas.getContext('2d');
 
     /**
      * @class tm.display.Label
@@ -30,6 +30,7 @@ tm.display = tm.display || {};
         /** @property _fontFamily @private */
         /** @property _fontWeight @private */
         /** @property _lineHeight @private */
+        /** @property _rowWidth @private */
         /** @property align */
         /** @property baseline */
         /** @property maxWidth */
@@ -40,7 +41,7 @@ tm.display = tm.display || {};
         init: function(text, size) {
             this.superInit();
             
-            this.text       = text || "";
+            this.text       = text;
             
             this._fontSize   = size || 24;
             this._fontFamily = tm.display.Label.default.fontFamily;
@@ -99,19 +100,76 @@ tm.display = tm.display || {};
          */
         _updateFont: function() {
             this.fontStyle = "{fontWeight} {fontSize}px {fontFamily}".format(this);
-            if (!dummyCanvas) {
-                dummyCanvas = document.createElement("canvas");
-                dummyContext = dummyCanvas.getContext('2d');
-            }
-            dummyContext.font = this.fontStyle;
-            this.textSize = dummyContext.measureText('あ').width * this.lineHeight;
+            
+            this.textSize = this.measure('あ') * this.lineHeight;
         },
 
         /**
          * @private
          */
         _updateLines: function() {
-            this._lines = (this._text+'').split('\n');
+            var lines = this._lines = (this._text + '').split('\n');
+
+            if (this._rowWidth) {
+                var rowWidth = this._rowWidth;
+                //どのへんで改行されるか目星つけとく
+                var defaultIndex = rowWidth / this.measure('あ') | 0;
+
+                for (var i = lines.length; i--;) {
+                    var text = lines[i], index, len, j = 0, remove = 1;
+                    while (true) {
+                        if (rowWidth > this.measure(text)) break;
+
+                        index = index || defaultIndex;
+                        len = text.length;
+                        if (len <= index) index = len - 1;
+
+                        if (rowWidth < this.measure(text.substring(0, index))) {
+                            while (rowWidth < this.measure(text.substring(0, --index)));
+                        } else {
+                            while (rowWidth >= this.measure(text.substring(0, ++index)));
+                            --index;
+                        }
+
+                        //index が 0 のときは無限ループになるので、1にしとく
+                        if (index === 0) index = 1;
+                        lines.splice(i + j++, 1, text.substring(0, index));
+                        lines.splice(i + j, 0, text = text.substring(index, len));
+                    }
+
+                }
+            }
+        },
+
+        /**
+         * このLabelインスタンスの設定で文字を描画したときの幅
+         * newLine true 指定で\nによる改行も考慮する
+         */
+        measure: function (text, newLine) {
+            dummyContext.font = this.fontStyle;
+            text = text == null ? '' : text + '';
+
+            if (newLine) {
+                text = text.split('\n');
+                var max = 0;
+
+                text.forEach(function (text) {
+                    var width = dummyContext.measureText(text).width;
+                    if (width > max) max = width;
+                });
+
+                return max;
+            }
+
+            return dummyContext.measureText(text).width;
+        },
+
+        /**
+         * 列の幅をセット
+         */
+        setRowWidth: function (v) {
+            this.rowWidth = v;
+            return this;
         },
         
     });
@@ -123,7 +181,7 @@ tm.display = tm.display || {};
     tm.display.Label.prototype.accessor("text", {
         "get": function() { return this._text; },
         "set": function(v){
-            if (v == null || v == undefined) {
+            if (v == null) {
                 this._text = "";
             }
             else {
@@ -168,6 +226,17 @@ tm.display = tm.display || {};
         "get": function() { return this._lineHeight; },
         "set": function(v) {
             this._lineHeight = v; this._updateFont();
+        },
+    });
+
+
+    /**
+     * @property rowWidth
+     */
+    tm.display.Label.prototype.accessor("rowWidth", {
+        "get": function () { return this._rowWidth; },
+        "set": function (v) {
+            this._rowWidth = v; this._updateLines();
         },
     });
     
